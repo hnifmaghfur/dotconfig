0b17d133df00e561f1db9295ec6c36c9dc15a814
Hanif Maghfur <hanifworks.ps@gmail.com>
2026-04-17 15:42:07 +0700

    feat: implement pane layout presets (main-horizontal, main-vertical, tiled)

--- wezterm/layouts.lua
+++ wezterm/layouts.lua
@@ -1,15 +1,39 @@
 local wezterm = require 'wezterm'
 local M = {}
 
+local function layout_main_horizontal(window, pane)
+  local tab = window:active_tab()
+  local panes = tab:panes()
+  if #panes == 1 then
+    pane:split { direction = 'Bottom', size = 0.35 }
+  end
+  window:perform_action(wezterm.action.PopKeyTable, pane)
+end
+
+local function layout_main_vertical(window, pane)
+  local tab = window:active_tab()
+  local panes = tab:panes()
+  if #panes == 1 then
+    pane:split { direction = 'Right', size = 0.35 }
+  end
+  window:perform_action(wezterm.action.PopKeyTable, pane)
+end
+
+local function layout_tiled(window, pane)
+  local tab = window:active_tab()
+  local panes = tab:panes()
+  if #panes == 1 then
+    local right = pane:split { direction = 'Right', size = 0.5 }
+    pane:split { direction = 'Bottom', size = 0.5 }
+    right:split { direction = 'Bottom', size = 0.5 }
+  end
+  window:perform_action(wezterm.action.PopKeyTable, pane)
+end
+
 function M.apply()
-  wezterm.on('layout-main-horizontal', function(window, pane)
-  end)
-
-  wezterm.on('layout-main-vertical', function(window, pane)
-  end)
-
-  wezterm.on('layout-tiled', function(window, pane)
-  end)
+  wezterm.on('layout-main-horizontal', layout_main_horizontal)
+  wezterm.on('layout-main-vertical', layout_main_vertical)
+  wezterm.on('layout-tiled', layout_tiled)
 end
 
 return M
